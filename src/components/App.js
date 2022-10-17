69af6315f6df13da3941259568e577131f8c7f37
jagritisingh3456 <108628421+jagritisingh3456@users.noreply.github.com>
2022-10-17 12:31:24 +0530

    button

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,38 @@ import "./../styles/App.css";
 function App() {
   return (
     <div id="main">
-      // Do not alter the main div
+    <p id="para"> Hello, I've learnt to use the full-stack evaluation
+     tool. This makes me so happy</p>
+        <button onClick={para}>Click</button>
     </div>
   );
 }
-
-
 export default App;
+
+
+// import React, { Component } from 'react';
+
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.createText = this.createText.bind(this);
+//   }
+
+
+//   createText() {
+//     return(
+//       <p>hello friend</p>
+//     )
+//   }
+
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <button onClick={this.createText}>Click</button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
