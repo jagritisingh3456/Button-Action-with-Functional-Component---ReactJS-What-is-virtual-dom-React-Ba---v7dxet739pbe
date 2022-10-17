76b9e88fd1c0d6d8a9f5c79009939d6ce21eefb4
Jagriti Singh <108628421+jagritisingh3456@users.noreply.github.com>
2022-10-17 12:52:15 +0530

    App.js
    code change

--- src/components/App.js
+++ src/components/App.js
@@ -1,41 +1,32 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
+// function App() {
+//   return (
+//     <div id="main">
+//     <p id="para"> Hello, I've learnt to use the full-stack evaluation
+//      tool. This makes me so happy</p>
+//         <button onClick={para}>Click</button>
+//     </div>
+//   );
+// }
+
+
+
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log("The new counter value is ", counter);
+  }, [counter]);
+
   return (
-    <div id="main">
-    <p id="para"> Hello, I've learnt to use the full-stack evaluation
-     tool. This makes me so happy</p>
-        <button onClick={para}>Click</button>
+    <div>
+      <h1>Counter</h1>
+      <p>current value: {counter}</p>
+      <button onClick={() => setCounter(counter + 1)}>Click</button>
     </div>
   );
 }
+
 export default App;
-
-
-// import React, { Component } from 'react';
-
-// class App extends Component {
-//   constructor() {
-//     super();
-//     this.createText = this.createText.bind(this);
-//   }
-
-
-//   createText() {
-//     return(
-//       <p>hello friend</p>
-//     )
-//   }
-
-
-//   render() {
-//     return (
-//       <div className="App">
-//         <button onClick={this.createText}>Click</button>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
